d10b8db06d00eaf4e0f9a0c6572c8f425ef7385d
Denise Pena <denise.pena@blackbaud.me>
2020-09-25 10:05:01 -0500

    Oops forgot to add JSdocs

--- src/app/public/modules/error/error-title.component.ts
+++ src/app/public/modules/error/error-title.component.ts
@@ -3,11 +3,19 @@ import {
   Input
 } from '@angular/core';
 
+/**
+ * Specifies a title to display in the error message.
+ */
 @Component({
   selector: 'sky-error-title',
   template: '<ng-content></ng-content>'
 })
 export class SkyErrorTitleComponent {
+/**
+ * Indicates whether to replace the default title. If `false`, this title
+ * is appended to the default title.
+ * @default false
+ */
   @Input()
   public replaceDefaultTitle: boolean = false;
 }
--- src/app/public/modules/error/error.component.ts
+++ src/app/public/modules/error/error.component.ts
@@ -23,6 +23,11 @@ import {
   templateUrl: './error.component.html'
 })
 export class SkyErrorComponent implements OnInit {
+/**
+ * Specifies an error type to use a set of pre-defined values for the image,
+ * title, and description. The valid options are `broken`, `construction`,
+ * `notfound`, and `security`.
+ */
   @Input()
   public set errorType(value: string) {
     this._errorType = value;
@@ -33,6 +38,10 @@ export class SkyErrorComponent implements OnInit {
     return this._errorType;
   }
 
+/**
+ * Indicates whether to display the error image.
+ * @default true
+ */
   @Input()
   public showImage: boolean = true;
 
